a94c241725849a5c9abde180ec45ecae5c7587e6
Christopher Van <cvan@mozilla.com>
2016-06-30 19:08:23 -0700

    Site updated: 2016-06-30 19:08:21

--- js/docs.js
+++ js/docs.js
@@ -1,11 +1,15 @@
 /* global $ */
-
 (function () {
 
 var anchorHeadingsSelector = 'h2[id], h3[id], h4[id], h5[id], h6[id]';
 
 var content = $('.content');
+
 if (content) {
+  if (window.location.hash) {
+    window.location.hash = window.location.hash.toLowerCase();
+  }
+
   content.addEventListener('click', function (e) {
     var el = e.target;
     if (el.matches && el.matches(anchorHeadingsSelector)) {
@@ -39,6 +43,15 @@ if (content) {
       }
     }
   });
+
+  // NOTE: Hack because variables doesn't get interpolated correctly in Markdown code blocks.
+  // This is for `/docs/<version>/guide/getting-started.html`
+  if (document.querySelector('#builds')) {
+    var versionEls = document.querySelectorAll('.highlight .code .string');
+    for (var i = 0; i < versionEls.length; ++i) {
+      versionEls[i].textContent = versionEls[i].textContent.replace('\{\{ version \}\}', settings.docsVersion);
+    }
+  }
 }
 
 })();
